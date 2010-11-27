35f472e6451d8c9be979f0e40da207f7d1cf4008
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2010-11-27 18:21:19 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nous.pylons.testing',
-    version='0.4.3',
+    version='0.4.4',
     description='Nous testing utils.',
     author='Ignas Mikalajunas',
     author_email='ignas@nous.lt',
